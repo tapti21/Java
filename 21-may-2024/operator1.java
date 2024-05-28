ec9ef9e1f1097d430f6cb9d642dbd0f7c719ce96
tapti21 <traptishakya21@gmail.com>
2024-05-28 16:52:37 +0530

    update commit

--- 21-may-2024/operator1.java
+++ 21-may-2024/operator1.java
@@ -1,39 +1,133 @@
 public class operator1 {
-    public static void main(String[] args) {
-        
-
-        //  Arithmatic Operators
-
-//      Addition, Subtraction, Multiplication, Division, Modulus;
-        int a = 24;
-        int b = 5;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        System.out.println(a%b);
-
-//      Increment
-        int c = 20;
-        c++;
-        System.out.println(c);
-
-        // int d = 20;
-        // ++a;
-        // System.out.println(d);
-
-
-//             Decrement
-        int d = 43;
-        d--;
-        System.out.println(d);
-
-
-//        Assignment Operators
-
-        
-
-       
+        public static void main(String[] args) {
+            
     
-}
-}
+            //  Arithmatic Operators
+    
+    //      Addition, Subtraction, Multiplication, Division, Modulus;
+            // int a = 24;
+            // int b = 5;
+            // System.out.println(a+b);
+            // System.out.println(a-b);
+            // System.out.println(a*b);
+            // System.out.println(a/b);
+            // System.out.println(a%b);
+    
+                    //.....Increment........
+            // int c = 20;
+            // c++;
+            // System.out.println(c);
+    
+            // int d = 20;
+            // ++a;
+            // System.out.println(d);
+    
+    
+                    //......Decrement.......
+            // int d = 43;
+            // d--;
+            // System.out.println(d);
+    
+    
+                    //......Assignment Operators.......
+    
+                    //..... Addition Assignment......
+            // int a = 5;
+            // a += 3; // Equivalent to a = a + 3; Now 'a' is 8
+            // System.out.println("Addition assignment, a = " + a);
+
+                        //..... Subtraction Assignment......
+
+            // int a = 8;
+            // a -= 2; // Equivalent to a = a - 2; Now 'a' is 6
+            // System.out.println("subtraction assignment, a = " + a);
+
+                             //.....Multiplication  Assignment......
+            // int a = 6;
+            // a *= 2; // Equivalent to a = a * 2; Now 'a' is 12
+            // System.out.println("Multiplication assignment, a = " + a);
+
+                            //..... Division  Assignment.....
+
+            // int a = 12;
+            // a /= 2; // Equivalent to a = a / 2; Now 'a' is 6
+            // System.out.println("Division assignment, a = " + a);
+
+                             //.....Modulus Assignment.....
+            // int a = 6;
+            // a %= 4; // Equivalent to a = a % 4; Now 'a' is 2
+            // System.out.println("Modulus assignment, a = " + a);
+
+
+            // int x = 5;
+            // x &= 3;
+            // System.out.println("End assignment, x = "+x);
+
+            // int x = 5;
+            // x |= 3;
+            // System.out.println(x);
+
+            // int x = 5;
+            // x ^= 3;
+            // System.out.println(x);
+
+            // int x = 5;
+            // x >>= 3;
+            // System.out.println(x);
+
+            // int x = 5;
+            // x <<= 3;
+            // System.out.println(x);
+
+
+                        //   ....Comparison Operators.......//
+                    
+                        // ....Equal to.....//   
+            // int x = 5;
+            // int y = 3;
+            // System.out.println(x == y); // returns false because 5 is not equal to 3
+
+                     // ....Not Equal to.....// 
+            // int x = 5;
+            // int y = 3;
+            // System.out.println(x != y); // returns true because 5 is not equal to 3
+
+                        // ....	Greater than.....// 
+            // int x = 5;
+            // int y = 3;
+            // System.out.println(x > y); // returns true because 5 is greater than 3
+
+                          // ....	Less than.....//
+            // int x = 5;
+            // int y = 3;
+            // System.out.println(x < y); // returns false because 5 is not less than 3
+
+                           // ....	Greater than or equal to.....//
+            // int x = 5;
+            // int y = 3;
+            // System.out.println(x >= y); // returns true because 5 is greater, or equal, to 3
+
+                            // ....	Less than or equal to.....//
+            // int x = 5;
+            // int y = 3;
+            // System.out.println(x <= y); // returns false because 5 is neither less than or equal to 3
+
+
+
+                                            // .....Logical Operator......//
+
+                         // ....Logical and....//
+            // int x = 5;
+            // System.out.println(x > 3 && x < 10); // returns true because 5 is greater than 3 AND 5 is less than 10
+            
+                       // ....Logical or....//
+            // int x = 5;
+            // System.out.println(x > 3 || x < 4); // returns true because one of the conditions are true (5 is greater than 3, but 5 is not less than 4)
+
+                          // ....Logical not....//
+            int x = 5;
+            System.out.println(!(x > 3 && x < 10)); // returns false because ! (not) is used to reverse the result        
+    }
+    }
+    
+
